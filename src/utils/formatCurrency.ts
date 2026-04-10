c91e6c03d4c3844708088d9b27237ccc8aa16766
Chritianregis <christiancheukap@gmailcom>
2026-04-10 11:48:26 +0100

    remove log to project

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -2,12 +2,6 @@
 // Fonctions utilitaires pour formater les montants
 export const formatCurrency = (amount: number): string => {
 
-  // Affiche le montant formaté dans la console pour vérifier le résultat
-  console.log(new Intl.NumberFormat('fr-FR', {
-    style: 'currency',
-    currency: 'CFA',
-  }).format(amount))
-
   return new Intl.NumberFormat('fr-FR', {
     style: 'currency',
     currency: 'CFA',
@@ -17,13 +11,6 @@ export const formatCurrency = (amount: number): string => {
 // Fonction pour formater les dates en français
 export const formatDate = (dateString: string): string => {
   const date = new Date(dateString)
-
-  // Affiche la date formatée dans la console pour vérifier le résultat
-  console.log(Intl.DateTimeFormat('fr-FR', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }).format(date))
 
 
   return new Intl.DateTimeFormat('fr-FR', {
